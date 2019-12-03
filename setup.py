e66a7b3ba06ced64172f153a0609b49c61aa534c
Piotr Rarus <piotr.rarus@softwarehut.com>
2019-12-03 15:17:37 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='nahre',
-    version='0.1.0',
+    version='0.1.1',
     author='Piotr Rarus',
     author_email='piotr.rarus@gmail.com',
     description='Computer vision research lib.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='http://msol-git:3000/ai-tools/nahre',
+    url='https://github.com/piotr-rarus/nahre',
     packages=setuptools.find_packages(
         exclude=[
             "tests"
